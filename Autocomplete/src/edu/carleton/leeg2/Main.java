bd80179e871c7516c6969b769efa882ed062dabb
Grant <emailgrantlee@gmail.com>
2018-03-27 17:54:17 -0500

    Added the Autocomplete Intellij project

---
+++ Autocomplete/src/edu/carleton/leeg2/Main.java
@@ -0,0 +1,63 @@
+package edu.carleton.leeg2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("      ooooooooooooooooo oooooooooooooooo oo oooooooooooooooooooooooooooo");
+        System.out.println("   o\"                                                                   \"$o");
+        System.out.println(" o\"                                                                       \"oo");
+        System.out.println("o\"           ,   .            ,--.           .    ,---.         .           \"o");
+        System.out.println("$            | . |            |    o         |      |           |             $");
+        System.out.println("o            | ) ) ,-. , , ,  |-   . ;-. ,-. |-     |   ;-. . . |             \"o");
+        System.out.println("$            |/|/  | | |/|/   |    | |   `-. |      |   |   | |               $");
+        System.out.println(" o           ' '   `-' ' '    '    ' '   `-' `-'    '   '   `-| o             $");
+        System.out.println(" \"o                                                         `-'             o\"");
+        System.out.println("   \"oo                                                                	  o\"");
+        System.out.println("      \"\" ooo o o   o o o o o o o o o o o   o  o   o o o        o o o oo\"");
+        System.out.println("                $ $ \" \" \" \" \" \"  \"  \"  \" \" \" \" \" \"     \"\" \"\" \"");
+        System.out.println("                o $");
+        System.out.println("                 \"$o");
+        System.out.println("                  \"$o");
+        System.out.println("                    \"");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("                   o$       o\"");
+        System.out.println("                  o\"$     $\"$    ooo\"\"     o");
+        System.out.println("                 $ o\"  oo\" $   o\"\" \"  oo$\"\" \"");
+        System.out.println("                $   $oo\"  \"\"$$\"     \"\"\"");
+        System.out.println("            oooo\"            o oo         oooooo");
+        System.out.println("            \"o         ooo\"\"\"");
+        System.out.println("            $$        \"                  o");
+        System.out.println("           o                             \" \"\"\"\"o");
+        System.out.println("      $o  o                                 $o");
+        System.out.println("      \"$o\"                                  o$");
+        System.out.println("      \"$\"                                    $o");
+        System.out.println("   \"\"\"\"\"                                      \"o ooo");
+        System.out.println("     ooo\" o$                 $$o             o$$\"   \"o");
+        System.out.println("    \"   o             o$$    \"\"\"              \"\"     $");
+        System.out.println("        \"o             \"$                      o  oo\"");
+        System.out.println("         $               o$o  \"\"o             o$$\"\"");
+        System.out.println("          $              $      $\"     oo$$\"  o$");
+        System.out.println("       o$\"$$              \"ooo$\"\"oo$$$$$\"\"  o$\"");
+        System.out.println("      $     oo        o oooo$$$$$$$$$$$\"   $\"");
+        System.out.println("      $     $$   $o$$$$$$$$$$\"\"\" \"$$$\"   $\"");
+        System.out.println("       $oooo$\"$o  \"\"\"\"\"$$$$$o     $\"   $\"                             ooooooo");
+        System.out.println("               \"$ooooo     \"\"\"\"\"\"$  o$\"                      ooo$$\"\"\"\"      \"$o");
+        System.out.println("                   \"\"\"$$oooooo$oo  \" \"$ooo         oooo$$\"$\"\"\"                $o");
+        System.out.println("                     o$$ $ $ $o\"o$\"$ $ o$$$\"\"$$\"$\"\"                           o$");
+        System.out.println("                 o o$$$\"       o o$$\"\"\"\"\" $o  $ \"$                       o oo\"\"");
+        System.out.println("            ooo$\"\"$$$$$$$$$\"$\"\"\"\"       $o \"     $$ooooo$ $o\"\"$\"\"\"\"\"\"\"\"\"\"\"\"");
+        System.out.println(" o$$\"$$$ooo$  $\" $$\"$$$$$$oooo$o$o\"\"\"\"$$\"\"o$oo$\"\"\"");
+        System.out.println(" $$ o$$$$$$$  \"\" o$o$$$\"\"\"\"\"\"\" \" \"\"\"\"\"$$$$$o\"");
+        System.out.println(" $o  $$$$\"\"\"\"$o$$$\"\"\"$o$\"\"\"\"\"\"\"\"\"\"\"\"\"\"$$");
+        System.out.println("  \"\"o\"\"             $$o$\"\"\"o\"o oooooooo$oo");
+        System.out.println("                    \"ooo$$oo oo o     o$\"");
+        System.out.println("                    \"$$$$$$$$$$$$$$$$$$\"");
+        System.out.println("            o$$\"\"\"\"\"$$$\"\"$$$$$$$$$$$$$$$oooooo");
+        System.out.println("      o$  o$\"         \"$oo o o\"$$$$ $$\"$$\"\"\"\"$$o    o");
+        System.out.println(" oo \"oo$$ $$o          \"\" \"\"\"  o$\" \"\"\"$        \"$$o$$  oo\"");
+        System.out.println("\"     \"\"\"$\"\"$$$$o$ooooooooooo\"\"$oooo o o o       o$$\"\"\"\" \"$\"$\"");
+        System.out.println("                                   \" \"\"\"\"\"\"\"$\"$\"$\"\"\"");
+    }
+}
