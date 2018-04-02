2e62605a9402d7a70dfb53c6cf9a82593e13140c
Grant <grant.lee.2017@gmail.com>
2018-04-02 14:01:13 -0500

    Beginning of Phase 2 of AutoCompleter Creation of Tests

--- Autocomplete/src/edu/carleton/leeg2/Autocompleter.java
+++ Autocomplete/src/edu/carleton/leeg2/Autocompleter.java
@@ -11,10 +11,16 @@ package edu.carleton.leeg2;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AutoCompleter {
+public class Autocompleter {
 
-
-    public AutoCompleter() {
+    /**
+     * @param dataFilePath the path to the data file containing the set of items to
+     * from which auto-completed results will be drawn. (In the context of this assignment,
+     * this will be the path to the actors.txt file I provided you. But we'll also talk
+     * later about how you might use inheritance to create subclasses of Autocompleter
+     * to use different datasets and different approaches to doing the autocompletion.)
+     */
+    public Autocompleter(String dataFilePath) {
         // Initialization goes here, as needed. For example, you might load
         // from a file into a list (or a hashmap or something like that)
         // the list of strings that are going to form the dataset of potential
---
+++ Autocomplete/src/edu/carleton/leeg2/AutocompleterTest.java
@@ -0,0 +1,22 @@
+package edu.carleton.leeg2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutocompleterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    //Edge Cases
+
+
+    //Basic Cases
+}
