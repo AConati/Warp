0cdfa258d6c73216c8a8b83585b747f1060fb586
Grant <grant.lee.2017@gmail.com>
2018-04-03 21:22:09 -0500

    fixed tests after comma notification

--- Autocomplete/src/edu/carleton/leeg2/Autocompleter.java
+++ Autocomplete/src/edu/carleton/leeg2/Autocompleter.java
@@ -1,6 +1,7 @@
 /**
  * Autocompleter.java
  * Jeff Ondich, 20 March 2018
+ * Ari Conati & Grant Lee
  *
  * This class exposes a very simple interface for generating auto-completions of search strings.
  * The purpose of this class is to give the students in CS257 an opportunity to practice creating
@@ -8,17 +9,11 @@
  */
 package edu.carleton.leeg2;
 
-import java.io.FileNotFoundException;
-import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.File;
-import java.util.Scanner;
 
 public class Autocompleter {
 
-
-    private List<Actor> actors;
     /**
      * @param dataFilePath the path to the data file containing the set of items to
      * from which auto-completed results will be drawn. (In the context of this assignment,
@@ -31,32 +26,8 @@ public class Autocompleter {
         // from a file into a list (or a hashmap or something like that)
         // the list of strings that are going to form the dataset of potential
         // auto-completions. The initialization will be up to you.
-        actors = new ArrayList<Actor>();
-        if(!load(dataFilePath, actors)) {
-            System.err.println("File not found.");
-            actors = null;
-        }
     }
 
-    private boolean load(String dataFilePath, List<Actor> actors) {
-
-        File file = new File(dataFilePath);
-        Scanner scanner = null;
-
-        try {
-            scanner = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            return false;
-        }
-
-        while(scanner.hasNextLine()) {
-            String name = scanner.nextLine();
-            Actor actor = new Actor(name);
-            actors.add(name);
-        }
-        return true;
-
-    }
     /**
      * @param searchString the string whose autocompletions are sought
      * @return the list of potential completions that match the search string,
@@ -66,49 +37,4 @@ public class Autocompleter {
     public List<String> getCompletions(String searchString) {
         return new ArrayList<String>();
     }
-
-    public static void printResults(List<String> list) {
-        for(String item : list) {
-            System.out.println(item);
-        }
-    }
-
-    public static void main(String[] args) {
-        if(!(args.length == 2)) {
-            System.err.println("Incorrect command line usage. Correct usage:" +
-                    "java Autocompleter pathToActorsFile searchString");
-            System.exit(0);
-        }
-        Autocompleter autocompleter = new Autocompleter(args[0]);
-        printResults(autocompleter.getCompletions(args[1]));
-    }
 }
-
-    /**
-     *
-     */
-    private static class Actor {
-        String fullName;
-        String refinedName;
-        int hierarchy;
-        int index;
-        int comma;
-
-        private Actor(String name) {
-            this.fullName = name;
-
-        }
-
-        private String refineName(String name) {
-            String refine = name.toLowerCase();
-            refine = Normalizer.normalize(refine, Normalizer.Form.NFD);
-            refine = refine.replaceAll("[^\\p{ASCII}]","");
-            refine = refine.replaceAll("\\p{M}", "");
-            refine.trim();
-            refine = refine.replaceAll("\\s\\-\\'", "");
-        }
-
-
-    }
-}
-
