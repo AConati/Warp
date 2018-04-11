00d2d82e8b79b8c5b161834135bff42b11e317ee
Ari Conati <conatia@carleton.edu>
2018-04-11 16:30:09 -0500

    Finished get_top_tracks method

--- UsingAPIs/api_test.py
+++ UsingAPIs/api_test.py
@@ -8,6 +8,7 @@
     and manage the potential errors.
 '''
 
+import subprocess
 import sys
 import argparse
 import json
@@ -58,26 +59,30 @@ def get_audio_analysis(song_id):
         result_list.append({'Song ID':song_id, 'Tempo of Song':song_tempo, 'Key of Track':song_key})
     return result_list
 
-def get_top_tracks (artist_id, country):
+def get_top_tracks (artist_id, country, token):
 	
-    base_url = 'https://api.spotify.com/v1/artists/{0}/top-tracks?country={1}'
-    url = base_url.format(artist_id, country)
-	
-    data_from_server = urllib.request.urlopen(url).read()
-    string_from_server = data_from_server.decode('utf-8')
-    track_list = json.loads(string_from_server)
-
+    base_url = 'https://api.spotify.com/v1/artists/{0}/top-tracks?country={1} -H "Authorization: Bearer {2}"'
+    url = base_url.format(artist_id, country, token)
+    track_list_string = subprocess.getoutput("GET " + url)
+    
+    #The dictionary returned uses false, true instead of False, True - change to correct version so Python can eval
+    track_list_string = track_list_string.replace("false", "False")
+    track_list_string = track_list_string.replace("true", "True")
+    
+    #Evaluate the output and store it as a dictionary variable
+    track_list = eval(track_list_string)
+    
     result_list = []
+    track_list = track_list['tracks']
     for track_dictionary in track_list:
         name = track_dictionary['name']
         popularity = track_dictionary['popularity']
-	
         result_list.append({'name':name, 'popularity':popularity})
 
     return result_list
 
 def main(args):
-    if args.action == 'analyze':
+    if args.request == 'analyze':
         song_analysis = get_audio_analysis(args.id)
         for track_info in song_analysis:
             song_track = track_info['track']
@@ -85,14 +90,12 @@ def main(args):
             song_key = track_info['key']
             print('{0} [{1} {2}]'.format(song_track, song_tempo, song_key))
 
-    elif args.action == 'conjugate':
-        conjugations = get_conjugations(args.word, args.language)
-        for conjugation in conjugations:
-            text = conjugation['text']
-            tense = conjugation['tense']
-	    person = conjugation['person']
-	    number = conjugation['number']
-            print('{0} [{1} {2} {3}]'.format(text, tense, person, number))
+    elif args.request == 'top':
+        tracks = get_top_tracks(args.id, args.country, args.token)
+        for track in tracks:
+            song_track = track['name']
+            song_popularity = track['popularity']  
+            print('{0} - popularity rating: {1}'.format(song_track, song_popularity))
     
 if __name__ == '__main__':
 
@@ -108,9 +111,12 @@ if __name__ == '__main__':
                         help='the id of what information is being requested about')
 
     parser.add_argument('country',
-			required = 'request' in sys.argv,
-		        help='the country of the market a particular artist\'s songs were popular in')
-
+			            metavar='country',
+		                help='the country of the market a particular artist\'s songs were popular in')
+    
+    parser.add_argument('token',
+                        metavar='token',
+                        help='Authorization token')
     args = parser.parse_args()                  
 
     main(args)
