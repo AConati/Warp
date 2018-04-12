fcad31cab8782a4fb286bf594bd1400001190277
Grant Lee <grant.lee.2017@gmail.com>
2018-04-12 12:50:45 -0500

    updated analyzesong in api_tester

--- UsingAPIs/api_test.py
+++ UsingAPIs/api_test.py
@@ -16,6 +16,7 @@ import urllib.parse
 import base64
 
 def get_audio_analysis(song_id):
+
     '''
     Returns a detailed audio analysis for a single track
     identified by a unique Spotify ID.
@@ -27,8 +28,8 @@ def get_audio_analysis(song_id):
     bars, sections, segments, tatums, and track in the following 
     style:
 	
-	{"bars": [{"start": 251.98282, "duration": 0.29765, "confidence": 0.652}],"meta":{
-	"analyzer_version:": "4.0.0", "platform": "Linux"...}}
+	{"bars": [{"start": 251.98282, "duration": 0.29765, "confidence":
+        0.652}],"meta":{"analyzer_version:": "4.0.0", "platform": "Linux"...}}
     
     The parameter "song_id" is a long combination of capital letters,
     lowercase letters, and numbers that can be found at the end of 
@@ -39,31 +40,31 @@ def get_audio_analysis(song_id):
     When the request is called incorrectly an error code and error object
     will most likely be returned.
     '''
-    base_url = 'https://api.spotify.com/v1/tracks/{0}'
+    #Formatting the URL Request with Header
+    base_url = 'https://api.spotify.com/v1/audio-analysis/{0}'
     url = base_url.format (song_id)
     request = urllib.request.Request(url)
     string_header_value = 'Bearer ' + getToken()
     request.add_header('Authorization', string_header_value)
     data_from_server = urllib.request.urlopen(request).read()
     string_from_server = data_from_server.decode('utf-8')
-    track_analysis_list = json.loads(string_from_server)
+    track_analysis_dict = json.loads(string_from_server)
     result_list = []
 
-    for track_analysis_dictionary in track_analysis_list:
-        print(track_analysis_dictionary)
-       #song_track = track_analysis_dictionary['track']
-       #song_tempo = song_track['tempo']
-       #song_key = song_track['key']
-        
-       # if type(song_id) != type(''):
-           # raise Exception('song_id has wrong type: "{0}"'.format(song_id))
-       # if type(song_tempo) != type(''):
-           # raise Exception('song_tempo has wrong type: "{0}"'.format(song_tempo))
-       # if type(song_key) != type(''):
-           # raise Exception('song_key has wrong type: "{0}"'.format(song_key))
+    #Retrieving the Requested from API's Return Dictionary (must be in str)
+    song_tempo = str(track_analysis_dict['track']['tempo'])
+    song_key = str(track_analysis_dict['track']['key'])
 
-       # result_list.append({'Song ID':song_id, 'Tempo of Song':song_tempo, 'Key of Track':song_key})
-   # return result_list
+    if type(song_id) != type(''):
+        raise Exception('song_id has wrong type: "{0}"'.format(song_id))
+    if type(song_tempo) != type(''):
+        raise Exception('song_tempo has wrong type: "{0}"'.format(song_tempo))
+    if type(song_key) != type(''):
+        raise Exception('song_key has wrong type: "{0}"'.format(song_key))
+
+    result_list.append({'song_id': song_id, 'song_tempo':song_tempo, 'song_key':song_key})
+    
+    return result_list.append
 
 def get_top_tracks (artist_id, country, token):
 	
@@ -118,11 +119,10 @@ def main(args):
 
     if args.request == 'analyze':
         song_analysis = get_audio_analysis(args.id)
-        for track_info in song_analysis:
-            song_track = track_info['track']
-            song_tempo = track_info['tempo']
-            song_key = track_info['key']
-            print('{0} [{1} {2}]'.format(song_track, song_tempo, song_key))
+        song_track = song_analysis['song_id']
+        song_tempo = song_analysis['song_tempo']
+        song_key = song_analysis['song_key']
+        print('{0}: tempo: {1}, key: {2}]'.format(song_track, song_tempo, song_key))
 
     elif args.request == 'top':
         tracks = get_top_tracks(args.id, args.country, getToken())
@@ -133,7 +133,7 @@ def main(args):
     
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Get song info from Spotify API')
+    parser = argparse.ArgumentParser(description='Get information from the Spotify API about tracks, artists, and more!')
 
     parser.add_argument('request',
                         metavar='request',
@@ -144,8 +144,9 @@ if __name__ == '__main__':
                         metavar='id',
                         help='the id of what information is being requested about')
 
-    parser.add_argument('country',
-		                metavar='country',
+    parser.add_argument('--country',
+                        #required 'request' == 'top',
+		        metavar= 'country',
                         help='the country of the market a particular artist\'s songs were popular in')
 
     args = parser.parse_args()                  
