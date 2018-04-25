f00bd2ebedd985e50d3ac03e1e226deae4c9006c
grant <grant.lee.2017@gmail.com>
2018-04-25 14:59:43 -0500

    Added Phase4

--- webapp/dataclean.R
+++ webapp/dataclean.R
@@ -1,7 +1,7 @@
 library(dplyr)
 
 data <- read.csv("assignments-grantIee/webapp/ny_phil.csv")
-data2 <- select(data, -c(eventType, id, Time, interval,  soloistRoles, season, programID, orchestra))
+data2 <- select(data, -c(eventType, id, Time, interval,  soloistRoles, season, programID, orchestra, movement))
 duplicate <- duplicated(data2)
 head(duplicate, n = 30)
 data3 <- data2[!duplicate, ]
@@ -10,6 +10,16 @@ noWorkTitle <- data3$workTitle == ""
 data4 <- data3[!noWorkTitle, ]
 data4$soloistName[data4$soloistName == ""] <- "No Soloist"
 data4$soloistInstrument[data4$soloistInstrument == ""] <- "Other"
+noSoloistInstrument <- data4$soloistInstrument == ""
+data5 <- data4[!noSoloistInstrument, ]
+data5$soloistName[91606]
+summary(data5)
+NASoloistInstrument <- is.na(data5$soloistInstrument)
+NASoloistInstrument
+data7 <- data6[!NASoloistInstrument, ] 
+
+summary(data7)
 
 
-write.csv(data4, file="assignments-grantIee/webapp/data2.csv")
+write.csv(data7, file="assignments-grantIee/webapp/data2.csv")
+
---
+++ webapp/phase4.md
@@ -0,0 +1,10 @@
+# Phase 4: Database Design and Population
+
+### **Authors**: Ari Conati & Grant Lee
+
+------
+
+|**Table Name**|**Command**|**Reasoning**|
+|:------------:|:---------:|:-----------:|
+|Performances  |
+
