77045561b49ddfb6893fb46c35ffae4d41e8f8d5
Ari Conati <conatia@carleton.edu>
2018-04-27 14:40:22 -0500

    Reformatted csv files

---
+++ webapp/reformat_csv2.py
@@ -0,0 +1,12 @@
+import sys
+
+read_file = open(sys.argv[1])
+count = 1
+for line in read_file:
+    line = line.rstrip()
+    line = line[line.index(",") + 1:]
+    line = str(count) +  "," + line 
+    count += 1
+    print(line)
+
+
