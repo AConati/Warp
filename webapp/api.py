0840c2d9616552fbb7371edce3b6d89c47d6c0e1
grant <grant.lee.2017@gmail.com>
2018-05-02 14:55:07 -0500

    api.py updated

--- webapp/api.py
+++ webapp/api.py
@@ -10,8 +10,41 @@
 import sys
 import flask
 import json
+import config
+import psycopg2
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_folder='static', template_folder='templates')
+
+def _fetch_all_rows_for_query(query):
+    '''
+    Returns a list of rows obtained from the books database by the specified SQL
+    query. If the query fails for any reason, an empty list is returned.
+
+    Note that this is not necessarily the right error-handling choice. Would users
+    of the API like to know the nature of the error? Do we as API implementors
+    want to share that information? There are many considerations to balance.
+    '''
+    try:
+        connection = psycopg2.connect(database=config.database, user=config.user, password=config.password)
+    except Exception as e:
+        print('Connection error:', e, file=sys.stderr)
+        return []
+
+    rows = []
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall() # This can be trouble if your query results are really big.
+    except Exception as e:
+        print('Error querying database:', e, file=sys.stderr)
+
+    connection.close()
+    return rows
+
+@app.after_request
+def set_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 @app.route('/')
 def hello():
@@ -22,6 +55,7 @@ def hello():
 @app.route('/soloists/')
 
 @app.route('/conductors')
+
 
 @app.route('/instruments')
 
@@ -35,7 +69,13 @@ def hello():
 
 @app.route('/performances/')
 
-
+@app.route('/help')
+def help():
+    rule_list = []
+    for rule in app.url_map.iter_rules():
+        rule_text = rule.rule.replace('<', '&lt;').replace('>', '&gt;')
+        rule_list.append(rule_text)
+    return json.dumps(rule_list)
 
 
 
