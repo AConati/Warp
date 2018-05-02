7e799105cb08ed453e46fe9a886a66b04aba7399
grant <grant.lee.2017@gmail.com>
2018-05-02 14:19:04 -0500

    made preliminary endpoint api.py

---
+++ webapp/api.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+	api.py
+	Ari Conati & Grant Lee
+	2 May 2018
+	
+	An api for retrieving data from 
+	PSQL database full of NY Phil Data
+"""
+import sys
+import flask
+import json
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def hello():
+	return 'Do you even vim?'
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	if len(sys.argv) != 3:
+		print('Usage: {0} hostport'.format(sys.argv[0]))
+		print('	Example: {0} perlman.mathcs.carleton.edu 5101'.format(sys.argv[0]))
+		exit()
+	
+	host = sys.argv[1]
+	port = int(sys.argv[2])
+	app.run(host=host, port=port, debug=True)
+
